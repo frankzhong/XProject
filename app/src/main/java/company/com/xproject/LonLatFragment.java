4ba2d6a6edc3881f13240a803a8b9847bce2104d
frankzhong <1033480608@qq.com>
2017-10-31 23:53:56 +0800

    添加了百度地图

--- app/src/main/java/company/com/xproject/LonLatFragment.java
+++ app/src/main/java/company/com/xproject/LonLatFragment.java
@@ -7,17 +7,51 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.TextureMapView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by zhongchao on 2017/10/30.
  */
 
 public class LonLatFragment extends android.app.Fragment{
+    @BindView(R.id.mTexturemap)
+    TextureMapView mMapView = null;
+    private BaiduMap mBaiduMap;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        SDKInitializer.initialize(this.getActivity().getApplicationContext());
         View view = inflater.inflate(R.layout.fragment_lonlat, container, false);
+        ButterKnife.bind(this, view);
+
+        mBaiduMap = mMapView.getMap();
+        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
 
         return view;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
     }
 }
--- app/src/main/java/company/com/xproject/ManageActivity.java
+++ app/src/main/java/company/com/xproject/ManageActivity.java
@@ -14,18 +14,18 @@ public class ManageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_manage);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView(R.layout.content_manage);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         int type = getIntent().getIntExtra(TYPE, 0);
         Fragment fragment = null;
