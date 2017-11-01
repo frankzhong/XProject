21764a72e9d320e26edf2a00fbad410d9d4ba12b
frankzhong <1033480608@qq.com>
2017-11-01 18:40:47 +0800

    添加了地理编码功能

--- app/src/main/java/company/com/xproject/LonLatFragment.java
+++ app/src/main/java/company/com/xproject/LonLatFragment.java
@@ -2,14 +2,24 @@ package company.com.xproject;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.TextureMapView;
+import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.search.core.SearchResult;
+import com.baidu.mapapi.search.geocode.GeoCodeOption;
+import com.baidu.mapapi.search.geocode.GeoCodeResult;
+import com.baidu.mapapi.search.geocode.GeoCoder;
+import com.baidu.mapapi.search.geocode.OnGetGeoCoderResultListener;
+import com.baidu.mapapi.search.geocode.ReverseGeoCodeResult;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,7 +31,16 @@ import butterknife.ButterKnife;
 public class LonLatFragment extends android.app.Fragment{
     @BindView(R.id.mTexturemap)
     TextureMapView mMapView = null;
+    @BindView(R.id.edt_address)
+    TextInputEditText mAddressEdit;
+    @BindView(R.id.text_lon)
+    TextView mLonText;
+    @BindView(R.id.text_lat)
+    TextView mLatText;
+    @BindView(R.id.btn_search)
+    Button mSearchBtn;
     private BaiduMap mBaiduMap;
+    private GeoCoder mSearch;
 
     @Nullable
     @Override
@@ -33,12 +52,26 @@ public class LonLatFragment extends android.app.Fragment{
         mBaiduMap = mMapView.getMap();
         mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
 
+        mSearch = GeoCoder.newInstance();
+        mSearch.setOnGetGeoCodeResultListener(listener);
+
+        mSearchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSearch.geocode(new GeoCodeOption()
+                        .city("深圳")
+                        .address("马家龙百货"));
+            }
+        });
+
+
         return view;
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        mSearch.destroy();
         //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
         mMapView.onDestroy();
     }
@@ -54,4 +87,32 @@ public class LonLatFragment extends android.app.Fragment{
         //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
         mMapView.onPause();
     }
+
+    OnGetGeoCoderResultListener listener = new OnGetGeoCoderResultListener() {
+
+        public void onGetGeoCodeResult(GeoCodeResult result) {
+
+            if (result == null || result.error != SearchResult.ERRORNO.NO_ERROR) {
+                //没有检索到结果
+                System.out.println("Error:"+result.error);
+                return;
+            }
+
+            LatLng latLng = result.getLocation();
+            mLatText.setText(String.valueOf(latLng.latitude));
+            mLonText.setText(String.valueOf(latLng.longitude));
+            //获取地理编码结果
+        }
+
+        @Override
+
+        public void onGetReverseGeoCodeResult(ReverseGeoCodeResult result) {
+
+            if (result == null || result.error != SearchResult.ERRORNO.NO_ERROR) {
+                //没有找到检索结果
+            }
+
+            //获取反向地理编码结果
+        }
+    };
 }
